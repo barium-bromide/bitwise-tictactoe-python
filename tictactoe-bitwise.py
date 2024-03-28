89510b664bc70330af6c4dd17c1f84c8a987e801
ddddddddrrdd3 <108572893+ddddddddrrdd3@users.noreply.github.com>
2024-03-28 20:52:03 +0800

    shorter version

--- tictactoe-bitwise.py
+++ tictactoe-bitwise.py
@@ -1,62 +1,27 @@
-boardState = 0
-PlayerX = 0
-PlayerO = 0
-input_position = 0
+boardState = PlayerO = PlayerX = input_position = 0
 player_turn_and_win_is_2 = 1 if input("X or O first?: ").lower() == "x" else 0
 
+def print_board(): print(*["".join(map(str,["-" if boardState & n != n else "X" if PlayerX & n == n else "O" for n in (1,2,4,8,16,32,64,128,256)][i: i+3])) for i in range(0,9,3)], sep="\n")
 while boardState != 511:
-    print(("-" if boardState & 1 != 1 else "X" if PlayerX & 1 == 1 else "O")
-           + ("-" if boardState & 2 != 2 else "X" if PlayerX & 2 == 2 else "O") 
-           + ("-" if boardState & 4 != 4 else "X" if PlayerX & 4 == 4 else "O") 
-           + "\n"
-           + ("-" if boardState & 8 != 8 else "X" if PlayerX & 8 == 8 else "O") 
-           + ("-" if boardState & 16 != 16 else "X" if PlayerX & 16 == 16 else "O") 
-           + ("-" if boardState & 32 != 32 else "X" if PlayerX & 32 == 32 else "O") 
-           + "\n" 
-           + ("-" if boardState & 64 != 64 else "X" if PlayerX & 64 == 64 else "O") 
-           + ("-" if boardState & 128 != 128 else "X" if PlayerX & 128 == 128 else "O")
-           + ("-" if boardState & 256 != 256 else "X" if PlayerX & 256 == 256 else "O"))
-    while (input_position := 2 ** (int(input("Key in the place you want to put (1 - 9): ")) - 1)) not in (1,2,4,8,16,32,64,128,256) or boardState & input_position > 0:
-        print("invalid move")
-
+    print_board()
+    while (input_position := 2 ** (int(input("Key in the place you want to put (1 - 9): ")) - 1)) not in (1,2,4,8,16,32,64,128,256) or boardState & input_position > 0: print("invalid move")
     boardState |= input_position
     match player_turn_and_win_is_2:
         case 0:
             PlayerO |= input_position
-            if PlayerO & 448 == 448 or PlayerO & 56 == 56 or PlayerO & 7 == 7 or PlayerO & 292 == 292 or PlayerO & 146 == 146 or PlayerO & 73 == 73 or PlayerO & 84 == 84 or PlayerO & 273 == 273:
-                print(("-" if boardState & 1 != 1 else "X" if PlayerX & 1 == 1 else "O")
-           + ("-" if boardState & 2 != 2 else "X" if PlayerX & 2 == 2 else "O") 
-           + ("-" if boardState & 4 != 4 else "X" if PlayerX & 4 == 4 else "O") 
-           + "\n"
-           + ("-" if boardState & 8 != 8 else "X" if PlayerX & 8 == 8 else "O") 
-           + ("-" if boardState & 16 != 16 else "X" if PlayerX & 16 == 16 else "O") 
-           + ("-" if boardState & 32 != 32 else "X" if PlayerX & 32 == 32 else "O") 
-           + "\n" 
-           + ("-" if boardState & 64 != 64 else "X" if PlayerX & 64 == 64 else "O") 
-           + ("-" if boardState & 128 != 128 else "X" if PlayerX & 128 == 128 else "O")
-           + ("-" if boardState & 256 != 256 else "X" if PlayerX & 256 == 256 else "O"))
+            if any(PlayerO & number == number for number in (448,56,7,292,146,73,84,273)):
+                print_board()
                 print("Player O won")
                 player_turn_and_win_is_2 = 2
                 break
         case 1:
             PlayerX |= input_position
-            if PlayerX & 448 == 448 or PlayerX & 56 == 56 or PlayerX & 7 == 7 or PlayerX & 292 == 292 or PlayerX & 146 == 146 or PlayerX & 73 == 73 or PlayerX & 84 == 84 or PlayerX & 273 == 273:
-                print(("-" if boardState & 1 != 1 else "X" if PlayerX & 1 == 1 else "O")
-           + ("-" if boardState & 2 != 2 else "X" if PlayerX & 2 == 2 else "O") 
-           + ("-" if boardState & 4 != 4 else "X" if PlayerX & 4 == 4 else "O") 
-           + "\n"
-           + ("-" if boardState & 8 != 8 else "X" if PlayerX & 8 == 8 else "O") 
-           + ("-" if boardState & 16 != 16 else "X" if PlayerX & 16 == 16 else "O") 
-           + ("-" if boardState & 32 != 32 else "X" if PlayerX & 32 == 32 else "O") 
-           + "\n" 
-           + ("-" if boardState & 64 != 64 else "X" if PlayerX & 64 == 64 else "O") 
-           + ("-" if boardState & 128 != 128 else "X" if PlayerX & 128 == 128 else "O")
-           + ("-" if boardState & 256 != 256 else "X" if PlayerX & 256 == 256 else "O"))
+            if any(PlayerX & number == number for number in (448,56,7,292,146,73,84,273)):
+                print_board()
                 print("Player X won")
                 player_turn_and_win_is_2 = 2
                 break
 
-    player_turn_and_win_is_2 = 1 - player_turn_and_win_is_2
+    player_turn_and_win_is_2 = 1-player_turn_and_win_is_2
 
-if player_turn_and_win_is_2 < 2:
-    print("draw")
+if player_turn_and_win_is_2 < 2:print("draw")
